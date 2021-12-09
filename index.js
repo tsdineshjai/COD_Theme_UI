83db777cebd7af7eba1eab0ead1d4185b0eb9b2c
tsdineshjai <tsdineshjai@gmail.com>
2021-12-09 20:05:19 +0530

    firstCommit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+const people = [
+    {
+        name: 'Mike Smith',
+        family: {
+            mother: 'Jane Smith',
+            father: 'Harry Smith',
+            sister: 'Samantha Smith'
+        },
+        age: 35
+    },
+    {
+        name: 'Tom Jones',
+        family: {
+            mother: 'Norah Jones',
+            father: 'Richard Jones',
+            brother: 'Howard Jones'
+        },
+        age: 25
+    }
+];
+
+for( const {age:Myage}, {family:{mother:ma}} of people){
+
+    console.log(`${Myage} & ${ma}`);
+}
